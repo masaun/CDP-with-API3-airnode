65e6c31d2a653bebfa9fe6c6092b563702001928
masaun <masanoriuno@gmail.com>
2021-06-07 08:53:37 +0900

    [SC]: fundWBTC()

--- contracts/CDP.sol
+++ contracts/CDP.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.6.12;
 
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 import { DAI } from "./mock-tokens/DAI.sol";
 import { WBTC } from "./mock-tokens/WBTC.sol";
@@ -10,7 +11,7 @@ import { WBTC } from "./mock-tokens/WBTC.sol";
 import { ExampleClient } from "./ExampleClient.sol";
 
 
-contract CDP {
+contract CDP is Ownable {
     using SafeMath for uint;
 
     uint currentLendId;
@@ -19,22 +20,32 @@ contract CDP {
     struct Lend {  // Also collateral
         uint daiAmountLended;
     }
-    mapping(address => mapping(uint => Lend)) lends;  // User address -> Lend ID 
     
     struct Borrow {
         uint wbtcAmountBorrowed;
     }
+
+    mapping(address => mapping(uint => Lend)) lends;  // User address -> Lend ID 
     mapping(address => mapping(uint => Borrow)) borrows;  // User address -> Borrow ID 
 
-
-    uint borrowLimitRate = 50;  // 50 (%) of collateral asset amount
+    // Rate
+    uint interestRateForLending = 5;     // 5 (%)  <- Calculated every block
+    uint interestRateForBorrowing = 10;  // 10 (%) <- Calculated every block
+    uint borrowLimitRate = 50;           // 50 (%) of collateral asset amount
 
     DAI public dai;
     WBTC public wbtc;
 
-    constructor(DAI _dai,WBTC _wbtc) public {
+    constructor(DAI _dai, WBTC _wbtc) public {
         dai = _dai;
         wbtc = _wbtc;
+    }
+
+    /**
+     * @notice - Fund WBTC for initial phase
+     */
+    function fundWBTC(uint fundWBTCAmount) public onlyOwner returns (bool) {
+        wbtc.transferFrom(msg.sender, address(this), fundWBTCAmount);
     }
 
     /**
@@ -52,21 +63,34 @@ contract CDP {
      * @param btcPrice - BTC/USD price that is retrieved via API3 oracle. (eg. bitcoin price is 35548 USD)
      */
     function borrowWBTC(uint lendId, uint btcPrice, uint borrowWBTCAmount) public returns (bool) {
-        Lend memory lend = lends[msg.sender][lendId];
-        uint daiAmountLended = lend.daiAmountLended;
-
         address borrower = msg.sender;
+
+        Lend memory lend = lends[borrower][lendId];
+        uint daiAmountLended = lend.daiAmountLended;  // Collateralized-amount
         uint borrowLimit = daiAmountLended.mul(btcPrice).div(1e18).mul(borrowLimitRate).div(100);
         require(borrowWBTCAmount <= borrowLimit, "WBTC amount borrowing must be less that the limit amount borrowing");
 
         wbtc.transfer(borrower, borrowWBTCAmount);
 
-        _borrow(borrowWBTCAmount);        
+        _borrow(borrowWBTCAmount);
     }
 
-    function repayWBTC() public returns (bool) {}
+    function repayWBTC(uint borrowId) public returns (bool) {
+        address borrower = msg.sender;
+        Borrow memory borrow = borrows[borrower][borrowId];
+        uint wbtcAmountBorrowed = borrow.wbtcAmountBorrowed;
 
-    function withdrawDAI() public returns (bool) {}
+        // [Todo]: Calculate interests amount of borrowing by every block
+        uint interestAmountForBorrowing;
+
+        // [Todo]: Calculate a repayment amount
+        uint repayAmount;  
+    }
+
+    function withdrawDAI() public returns (bool) {
+        // [Todo]: Calculate earned-interests amount of lending by every block
+        uint interestAmountForLending;
+    }
 
 
     //------------------
